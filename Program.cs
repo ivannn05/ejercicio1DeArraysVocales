6a455da49cd0870448b131ab0a6178349530e799
DESKTOP-M9FDRN9\csi23-iloposa <csi23-iloposa@colegioaltair.net>
2023-12-12 14:41:15 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ejercicio1DeArraysVocales;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+
+
+        char[] vocales2 = { 'a', 'e', 'i', 'o', 'u' };
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (vocales[i] == vocales2[i])
+            {
+               
+            }
+            else  Console.WriteLine(i);
+
+            
+        }
+    }
+}
